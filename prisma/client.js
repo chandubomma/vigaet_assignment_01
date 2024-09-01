81bd955555a157f940451fdc7f8edb13b82bc574
chandubomma <bommachandugoud@gmail.com>
2024-09-01 14:08:57 +0530

    add prisma middleware for password hashing

--- prisma/client.js
+++ prisma/client.js
@@ -1,5 +1,19 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
+
+
+prisma.$use(async (params, next) => {
+    if (params.model === 'User' && (params.action === 'create' || params.action === 'update')) {
+      const user = params.args.data;
+      if (user.password) {
+        const saltRounds = 10;
+        user.password = await bcrypt.hash(user.password, saltRounds);
+      }
+    }
+  
+    return next(params);
+  });
 
 export default prisma;
